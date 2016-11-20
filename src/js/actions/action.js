9b956ce2479c84b811413fa6fd52f61e88dbac47
Jay Kumar <jaykch@outlook.com>
2016-11-20 17:23:16 +0000

    Finished recent and all time toggle functionality

--- src/js/actions/action.js
+++ src/js/actions/action.js
@@ -1,9 +1,22 @@
 import axios from "axios";
 
-export function fetchUsers(){
+export function fetchRecentUsers(){
 
     return function(dispatch){
         axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/recent")
+            .then((response) => {
+                dispatch({type: "USERS_RECEIVED", payload: response.data})
+            })
+            .catch((err) => {
+                dispatch({type: "FETCHING_FAILED", payload: err})
+            })
+    }
+}
+
+export function fetchAlltimeUsers(){
+
+    return function(dispatch){
+        axios.get("https://fcctop100.herokuapp.com/api/fccusers/top/alltime")
             .then((response) => {
                 dispatch({type: "USERS_RECEIVED", payload: response.data})
             })
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,7 +4,7 @@ import {connect} from "react-redux"
 
 import Footer from "./Footer"
 
-import {fetchUsers} from "../actions/action"
+import {fetchRecentUsers, fetchAlltimeUsers} from "../actions/action"
 
 @connect((store) => {
     return {
@@ -15,30 +15,37 @@ import {fetchUsers} from "../actions/action"
 
 export default class Layout extends React.Component {
 
-    fetchUsers() {
-        this.props.dispatch(fetchUsers());
+    componentWillMount() {
+        this.props.dispatch(fetchAlltimeUsers());
+    }
+
+    fetchRecentUsers(){
+        this.props.dispatch(fetchRecentUsers());
+    }
+
+    fetchAlltimeUsers(){
+        this.props.dispatch(fetchAlltimeUsers());
     }
 
     render() {
-        console.log(this.props.users);
-
-        if (this.props.users.length == 0) {
-            return <button className="btn btn-primary" onClick={this.fetchUsers.bind(this)}>Load Users</button>
-        }
-
-        const mapUsers = this.props.users.map(user =>
-         <li>
-         <h3>{user.username}</h3>
-         </li>
-         );
+        const users = this.props.users;
+        const mapUsers = users.map(user =>
+            <li className="user" key={user.username}>
+                <img src={user.img} alt={user.username}/>
+                <span className="recent">{user.recent}</span>
+                <span className="all-time">{user.alltime}</span>
+                <h3>{user.username}</h3>
+            </li>
+        );
 
         return (
             <div class="container">
                 <h1 class="text-center header">Camper Leaderboard</h1>
-                <ul className="user-list">
+                <button onClick={this.fetchRecentUsers.bind(this)} className="btn btn-block btn-primary">Recent</button>
+                <button onClick={this.fetchAlltimeUsers.bind(this)} className="btn btn-block btn-primary">Alltime</button>
+                <ul className="user-list container">
                     {mapUsers}
                 </ul>
-
                 <Footer/>
             </div>
         )
